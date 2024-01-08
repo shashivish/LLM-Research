a3466fd4bafdb699ab4659a99409f1052ba9db10
shashi.vish <shashi.vish@yahoo.in>
2024-01-08 14:49:25 +0800

    graph plotting

--- GraphNeuralNetwork/plot_multiple_graph.py
+++ GraphNeuralNetwork/plot_multiple_graph.py
@@ -8,21 +8,27 @@ G = nx.DiGraph()
 edges = [('A', 'B'), ('B', 'C'), ('E', 'F')]
 G.add_edges_from(edges)
 
-# Find connected components in the graph
-# For a directed graph, we use weakly connected components
-subgraphs = [G.subgraph(c).copy() for c in nx.weakly_connected_components(G)]
+G_undirected = nx.Graph()
+
+# Add edges to the graph
+G_undirected.add_edges_from(edges)
+
+# Find connected components in the undirected graph
+subgraphs_undirected = [G_undirected.subgraph(c).copy() for c in nx.connected_components(G_undirected)]
 
 # Create a figure with subplots based on the number of subgraphs
-n_subgraphs = len(subgraphs)
-fig, axs = plt.subplots(1, n_subgraphs, figsize=(6 * n_subgraphs, 6))
+n_subgraphs_undirected = len(subgraphs_undirected)
+fig, axs = plt.subplots(1, n_subgraphs_undirected, figsize=(6 * n_subgraphs_undirected, 6))
 
 # Check if there is only one subplot (i.e., one subgraph)
-if n_subgraphs == 1:
+if n_subgraphs_undirected == 1:
     axs = [axs]
 
-# Draw each subgraph in a separate subplot
-for i, sg in enumerate(subgraphs):
-    nx.draw(sg, with_labels=True, node_color='lightblue', edge_color='black', ax=axs[i])
+# Draw each subgraph with edge labels in a separate subplot
+for i, sg in enumerate(subgraphs_undirected):
+    pos = nx.spring_layout(sg)  # Define a layout for nodes
+    nx.draw(sg, pos, with_labels=True, node_color='lightblue', edge_color='black', ax=axs[i])
+    nx.draw_networkx_edge_labels(sg, pos, edge_labels={(u, v): f'{u} - {v}' for u, v in sg.edges()}, ax=axs[i])
     axs[i].set_title(f'Subgraph {i+1}')
 
 plt.tight_layout()
